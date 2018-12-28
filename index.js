56b02833adc29612c44e53536566238135444f59
vyskoutaris <vyskoutaris@hotmail.com>
2018-12-28 13:30:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,13 +4,6 @@ var recipes = { foo: 'bar' }
 var newObj = Object.assign({}, obj)
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, obj)
+  return Object.assign({}, object, )
 }
- 
-newObj // { foo: 'bar' }
- 
-delete newObj.foo // true
- 
-newObj // {}
- 
-obj // { foo: 'bar' }
+

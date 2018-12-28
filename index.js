42d672152e6ab76f55f078d582d44d0321f12a0b
vyskoutaris <vyskoutaris@hotmail.com>
2018-12-28 15:11:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,4 @@
 
 var recipes = { foo: 'bar' }
 
-function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, {[key]: value});
-}
-
-function deleteFromObjectByKey(object, key) {
-  delete Object[key];
-  return Object;
-} 
-
-function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object[key];
-  return object;
-}
-
-function destructivelyDeleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, object);
-  delete newObj[key];
-  return newObj;
-}
 
